8b2ad9e6e4ad1f4c752ada9c69c91efeeee58296
Tankakuka <fjb65@scarletmail.rutgers.edu>
2023-03-09 18:35:03 -0500

    Sprites applied to ScriptableObject

--- Assets/Prospector/__Scripts/CardSpritesSO.cs
+++ Assets/Prospector/__Scripts/CardSpritesSO.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "CardSprites", menuName = "ScriptableObjects/CardSpriteSO")]
-public class CardSpritesSO : MonoBehaviour
+public class CardSpritesSO : ScriptableObject
 {
     [Header("Card Stock")]
     public Sprite cardBack;
